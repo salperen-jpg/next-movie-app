29cf0ceb1af75b5212d6da87a541c95a1ab7258f
Salih Alperen <sal.alperen1@gmail.com>
2022-03-26 00:11:26 +0100

    icons added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from '../styles/Navbar.module.scss';
+import { MdMovie, MdOutlineSubscriptions } from 'react-icons/md';
+import { CgProfile } from 'react-icons/cg';
 
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
       <div className={styles.navcenter}>
         <h3>
-          HOLA<span>AA</span>
+          HO<span className={styles.letter}>LA</span>
         </h3>
         <div className={styles.rightnav}>
           <ul className={styles.navlinks}>
-            <li>About</li>
-            <li>Search</li>
+            <li>
+              <MdMovie className={styles.animation}></MdMovie>
+            </li>
+            <li>
+              <CgProfile></CgProfile>
+            </li>
+            <li>
+              <MdOutlineSubscriptions></MdOutlineSubscriptions>
+            </li>
           </ul>
         </div>
       </div>
